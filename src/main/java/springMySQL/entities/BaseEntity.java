b303341c126e5dcaeb1334a12f8ee8d8c490ac03
DawidMk <dawid_m@interia.pl>
2019-06-10 20:44:24 +0200

    updated BaseEntity

--- src/main/java/springMySQL/entities/BaseEntity.java
+++ src/main/java/springMySQL/entities/BaseEntity.java
@@ -2,17 +2,17 @@ package springMySQL.entities;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
 @MappedSuperclass
-@Getter
-@Setter
 public class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//            , generator = "native")
+//    @GenericGenerator(name = "native", strategy = "native")
     @Column(name = "id")
-    private Integer id;
+    @Getter
+    @Setter
+    protected Integer id;
 }
